daca9e1fc866bfa5285c8ef330baf76085b7fbc5
insert github real name here <insert github email here>
2021-08-10 22:08:01 -0700

    correct substract test

--- tests/src/test_calculator.py
+++ tests/src/test_calculator.py
@@ -6,7 +6,5 @@ def test_add():
     assert cal.add(1, 1) == 2
     assert cal.add(1, 2) == 3
 def test_divide():
-    assert cal.div(3, 1) == 3
-
-def test_divide():
+    assert cal.divide(3, 1) == 3
     assert cal.divide(2,1) == 2 
